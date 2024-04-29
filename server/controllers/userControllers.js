c006257f98948e67da43ef4901645bad96c25916
evansutjahjo <evansutjahjo@gmail.com>
2024-04-29 20:49:27 +0800

    usercontroller authorization middleware

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,8 @@
 const bcrypt = require ('bcryptjs')
 const jwt = require ('jsonwebtoken')
+const fs = require ('fs')
+const path = require('path')
+const {v4: uuid} = require ('uuid')
 
 const User = require ('../models/userModel')
 const HttpError = require("../models/errorModel");
@@ -64,19 +67,105 @@ const loginUser = async (req,res, next) => {
 }
 
 const getUser = async (req,res, next) => {
-  res.json("User Profile")
+  try {
+      const {id} = req.params;
+      const user = await User.findById(id).select(`-password`);
+      if(!user) {
+        return next(new HttpError("User not found", 404))
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      return next(new HttpError(error))
+  }
 }
 
 const changeAvatar = async (req,res, next) => {
-  res.json("Change User Avatar")
+  try {
+    if(!req.files.avatar) {
+      return next (new HttpError("Please upload an image", 422))
+    }
+
+    const user = await User.findById(req.user.id)
+    if (user.avatar) {
+      fs.unlink(path.join(__dirname, '..', 'uploads',user.avatar), (err) => {
+        if (err) {
+          return next (new HttpError(err))
+        }
+      })
+    }
+    const {avatar} = req.files;
+    if (avatar.size > 500000) {
+      return next (new HttpError ("File Upload exceeds 500kb limit"),422)
+    }
+
+    let fileName;
+    fileName = avatar.name;
+
+    let splittedFilename = fileName.split('.')
+    let newFilename = splittedFilename[0] + uuid() + '.' + splittedFilename[splittedFilename.length - 1]
+
+    avatar.mv(path.join(__dirname, '..', 'uploads', newFilename),async (err) => {
+      if (err) {
+        return next (new HttpError(err))
+      }
+
+      const updatedAvatar = await User.findByIdAndUpdate(req.user.id, {avatar: newFilename}, {new: true})
+      if(!updatedAvatar){
+        return next (new HttpError("Avatar couldn't be changed",422))
+      }
+      res.status(200).json(updatedAvatar)
+    })
+
+  } catch (error) {
+    return next (new HttpError(error))
+  }
 }
 
 const editUser = async (req,res, next) => {
-  res.json("Edit User Details")
+  try {
+    const {name, email, currentPassword, newPassword, confirmNewPassword} = req.body;
+    if(!name || !email || !currentPassword || !newPassword){
+      return next (new HttpError("Fill in all the fields", 422))
+    }
+
+    const user = await User.findById(req.user.id);
+    if(!user){
+      return next (new HttpError ("User not found",403))
+    }
+
+    const emailExist = await User.findOne({email});
+    if (emailExist && (emailExist._id != req.user.id)) {
+      return next (new HttpError ("Email already exist", 422))
+    }
+
+    const validateUserPassword = await bcrypt.compare(currentPassword, user.password);
+    if(!validateUserPassword) {
+      return next(new HttpError ("Invalid current password", 422))
+    }
+
+    if (newPassword !== newConfirmNewPassword){
+      return next (new HttpError ("New Password does not match", 422))
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(newPassword, salt)
+
+    const newInfo = await User.findByIdAndUpdate(req.user.id, {name,email, password:hash}, {new:true})
+    res.status(200).json(newInfo)
+
+  } catch (error) {
+    return next (new HttpError (error))
+  }
 }
 
 const getMembers = async(req, res,next) =>{
-  res.json("get all users/members")
+  try {
+    const members=await User.find().select('-password');
+    res.json(members);
+  } catch (error) {
+    return next (new HttpError(error))
+
+  }
 }
 
 module.exports = {registerUser, loginUser, getMembers,getUser,changeAvatar, editUser}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,13 +2,15 @@ const {Router} = require('express')
 
 const {registerUser, loginUser, getMembers,getUser,changeAvatar, editUser} = require ("../controllers/userControllers")
 
+const authMiddleware = require ('../middleware/authMiddleware')
+
 const router = Router()
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/:id',getUser)
 router.get('/', getMembers)
-router.post('/change-avatar', changeAvatar)
-router.patch('/edit-user',editUser)
+router.post('/change-avatar', authMiddleware, changeAvatar)
+router.patch('/edit-user', authMiddleware, editUser)
 
 module.exports = router
